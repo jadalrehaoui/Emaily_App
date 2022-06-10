555649916918afeedb52e93daac9189aa7160daf
jad rehaoui <jrehaoui.dev@outlook.com>
2022-06-10 11:55:05 -0600

    changed port config from env

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use('/api', API);
 // just a response with 404 statusCode and a message
 app.use("*", middlewares.m404);
 // starting to listed to requests on the port in .env or in the deployment env port specified
-app.listen(process.env.PORT, error => {
+app.listen(process.env.PORT || 3000, error => {
   error
-  ? 
+  ?
   console.log("!!=> Something went wrong, could not launch server on", process.env.PORT)
   :
   console.log("=> NodeJS/ExpressJS running on port", process.env.PORT)
