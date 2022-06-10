607a23e579cf4ca67b39a7922e95c3bc3aa4e602
jad rehaoui <jrehaoui.dev@outlook.com>
2022-06-10 11:59:27 -0600

    changed PORT

--- index.js
+++ index.js
@@ -3,18 +3,19 @@ const cors = require('cors');
 const API = require('./api');
 const middlewares = require('./middlewares');
 // env
-require('dotenv').config();
+// require('dotenv').config();
 // cross origin enabled
 app.use(cors());
 // all our /api calls gets routed here
 app.use('/api', API);
 // just a response with 404 statusCode and a message
 app.use("*", middlewares.m404);
+const PORT = process.env.PORT || 3000;
 // starting to listed to requests on the port in .env or in the deployment env port specified
-app.listen(process.env.PORT || 3000, error => {
+app.listen(PORT, error => {
   error
   ?
-  console.log("!!=> Something went wrong, could not launch server on", process.env.PORT)
+  console.log("!!=> Something went wrong, could not launch server on", PORT)
   :
-  console.log("=> NodeJS/ExpressJS running on port", process.env.PORT)
+  console.log("=> NodeJS/ExpressJS running on port", PORT)
 })
